ee3decedd86758b44ffae3924f8b1aac56ca3958
KrishaPriya <krishna88priya@gmail.com>
2022-03-20 12:45:27 +0530

    update the retry count for the init drvier

--- IctakTechBlog/src/test/java/Scripts/TestBase.java
+++ IctakTechBlog/src/test/java/Scripts/TestBase.java
@@ -19,6 +19,7 @@ public class TestBase {
     public static Properties prop = null;
     public WebDriver driver;
     public static String driverpath = "";
+    public static int retryCount = 0;
 
     public static void TestBase() {
         try {
@@ -36,27 +37,36 @@ public class TestBase {
 
     @Parameters("browser")
     @BeforeClass
-    public void onSetup(String browserName) throws InterruptedException {
-      //  Logger logger= LogManager.getLogger(TestBase.class);
-        Thread.currentThread().getStackTrace();
-
-        System.out.println("onSetup is called....");
+    public void onSetup(String browserName) {
         TestBase();
-        if (browserName.equals("chrome")) {
-            System.setProperty("webdriver.chrome.driver", driverpath);
-            driver = new ChromeDriver();
+        System.out.println("onSetup is called....");
+        try {
+            if (browserName.equals("chrome")) {
+                System.setProperty("webdriver.chrome.driver", driverpath);
+                driver = new ChromeDriver();
+            }
+            if (browserName.equals("firefox")) {
+                System.setProperty("webdriver.gecko.driver", driverpath);
+                driver = new FirefoxDriver();
+            }
+            driver.get(prop.getProperty("url"));
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            WebDriverWait wait = new WebDriverWait(driver,5);
+            wait.until(ExpectedConditions.urlToBe(prop.getProperty("url")));
+        }catch (Exception e) {
+            if (retryCount < 5){
+                retryCount ++;
+                onSetup(browserName);
+                return;
+            }
+            System.out.println("Failed to setup webderiver");
         }
-        if (browserName.equals("firefox")) {
-            System.setProperty("webdriver.gecko.driver", driverpath);
-            driver = new FirefoxDriver();
-        }
-        driver.get(prop.getProperty("url"));
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        WebDriverWait wait = new WebDriverWait(driver,5);
-        wait.until(ExpectedConditions.urlToBe(prop.getProperty("url")));
+
+
+
 //        driver.manage().window().maximize();
-        driver.manage().window().setPosition(new Point(1024,0));
-        driver.manage().window().setSize(new Dimension(1024,728));
+//        driver.manage().window().setPosition(new Point(1024,0));
+//        driver.manage().window().setSize(new Dimension(1024,728));
     }
 
 
